bbc723f5acffa78893fca96192458706a93b5d08
pitu <ashishkumarvish.432@gmail.com>
2021-11-11 15:59:13 +0530

    BinarySearchTree

---
+++ src/com/blz/binarytree/MyBinaryTreeMain.java
@@ -0,0 +1,8 @@
+package com.blz.binarytree;
+
+public class MyBinaryTreeMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to binary search tree program");
+        
+    }
+}
