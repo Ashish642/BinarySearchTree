1586682dd26fbc53a8f55b740df3ec96800377b9
pitu <ashishkumarvish.432@gmail.com>
2021-11-11 17:49:56 +0530

    Searching method

--- src/com/blz/binarytree/MyBinaryTree.java
+++ src/com/blz/binarytree/MyBinaryTree.java
@@ -17,25 +17,42 @@ public class MyBinaryTree<K extends  Comparable<K>> {
             return current;
 
         }
-        public void add(K key) {
-            this.root = this.addRecursively(root,key);
+        public void add (K key){
+            this.root = this.addRecursively(root, key);
 
         }
-        private int getSizeRecursively(MyBinarynode<K> current) {
-            return current == null ? 0 : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+        private boolean searchTreeRecursively (MyBinarynode < K > current, K key){
+            if (current == null) {
+                return false;
+            }
+            int compareResult = key.compareTo(current.key);
+            if (compareResult == 0) {
+                return true;
+
+            }
+            return compareResult < 0 ? searchTreeRecursively(current.left, key) : searchTreeRecursively(current.right, key);
+
 
         }
-        public int getSize() {
+        public boolean searchTree (K key){
+            return searchTreeRecursively(root, key);
+
+        }
+        private int getSizeRecursively (MyBinaryNode < K > current) {
+            return current == null ? 0 : +this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+        }
+        public int getSize () {
             return this.getSizeRecursively(root);
 
         }
-        public void printBinaryTree() {
+        public void printBinaryTree () {
             System.out.println("My Tree: " + root);
 
         }
         @Override
-                public String toString() {
-            return "MyBinaryTree{" + root + '}'
+        public String toString () {
+            return "MyBinaryTree{" + root + '}';
         }
 
+    }
 }
