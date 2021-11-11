2e2416aa9ced01081e57bcbe023f26134a863716
pitu <ashishkumarvish.432@gmail.com>
2021-11-11 16:58:45 +0530

    Creating binary search tree by add

--- src/com/blz/binarytree/MyBinaryTreeMain.java
+++ src/com/blz/binarytree/MyBinaryTreeMain.java
@@ -3,6 +3,13 @@ package com.blz.binarytree;
 public class MyBinaryTreeMain {
     public static void main(String[] args) {
         System.out.println("Welcome to binary search tree program");
-        
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+
+        myBinaryTree.printBinaryTree();
+
     }
 }
